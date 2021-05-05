c4cd02cf28fc8b00f52539a5015584da1e7ead16
meenakshirana8587 <69763066+meenakshirana8587@users.noreply.github.com>
2021-05-05 16:19:18 +0530

    code first approach and Product api

---
+++ prodcrudapi2/prodcrudapi2/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using prodcrudapi2.Infrastructure;
+using prodcrudapi2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prodcrudapi2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductContext _context;
+        public ProductController(ProductContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("items")]
+        public List<Product> getItems()
+        {
+            var res = from r in _context.Products select r;
+            return res.ToList();
+        }
+        [HttpPost("items")]
+        public Product createItems(Product item)
+        {
+            _context.Products.Add(item);
+            _context.SaveChanges();
+            return item;
+        }
+
+
+        [HttpPut("items/{id}")]
+        public Product updateItems(Product item)
+        {
+
+            _context.Entry(item).State = EntityState.Modified;
+            _context.SaveChanges();
+            return item;
+
+        }
+    }
+}
---
+++ prodcrudapi2/prodcrudapi2/Infrastructure/ProductContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using prodcrudapi2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prodcrudapi2.Infrastructure
+{
+    public class ProductContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+
+        }
+    }
+}
